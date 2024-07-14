1e152a3a7498dd8bf31a26906e9ff9cb67b684e8
ttrrayl <2011522010_tri@student.unand.ac.id>
2024-07-14 23:23:08 +0700

    detail already done but not so done

--- app/src/main/java/com/example/kumandra/ui/DetailStoryActivity.kt
+++ app/src/main/java/com/example/kumandra/ui/DetailStoryActivity.kt
@@ -4,27 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.example.kumandra.data.remote.response.ListStoryItem
+import com.example.kumandra.data.remote.response.Report
 import com.example.kumandra.databinding.ActivityDetailStoryBinding
 
 class DetailStoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailStoryBinding
-    private lateinit var detailStory: ListStoryItem
+    private lateinit var detailReport: Report
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+      //  supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        detailStory = intent.getParcelableExtra<ListStoryItem>(STORY_DETAIL) as ListStoryItem
+        detailReport = intent.getParcelableExtra<Report>(STORY_DETAIL) as Report
         binding.apply {
             Glide.with(applicationContext)
-                .load(detailStory.photoUrl)
+                .load(detailReport.image_url)
                 .into(ivDetail)
-            tvName.text = detailStory.name
-            tvDeskripsi.text = detailStory.description
-            tvTanggalDetail.text = detailStory.createdAt
+            tvCreated.text = ":" + detailReport.created_at
+            tvUpdate.text =  ":" + detailReport.updated_at
+            tvEmail.text =  ":" + detailReport.email
+            tvFacil.text =  ":" + detailReport.nama_detail_facilities
+            tvClass.text =  ":" + detailReport.nama_classes
+            tvDesc.text =  ":" + detailReport.description
         }
 
     }
